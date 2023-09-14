78bb4dc6907b6c6b071ea5eefe1e6e37a17e6a09
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-09-14 13:58:02 -0500

    [imp] (films mic-serv) FAKE_CRUD✅

---
+++ films/src/data/index.js
@@ -0,0 +1,47 @@
+const { ClientError } = require("../utils/errors");
+const films = require("./films.json");
+
+// DB simulation
+const list = async () => {
+  return films;
+};
+
+const create = async (film) => {
+  const index = films.findIndex((f) => f.id === film.id);
+  if (index !== -1) {
+    throw new ClientError("Film already exists", 400);
+  }
+  films.push(film);
+};
+
+const findById = async (id) => {
+  const film = films.find((film) => film.id === id);
+  if (!film) {
+    throw new ClientError("Film not found", 404);
+  }
+  return film;
+};
+
+const findByIdAndRemove = async (id) => {
+  const index = films.findIndex((film) => film.id === id);
+  if (index === -1) {
+    throw new ClientError("Film not found", 404);
+  }
+  films.splice(index, 1);
+};
+
+const findByIdAndUpdate = async (id, body) => {
+  const index = films.findIndex((film) => film.id === id);
+  if (index === -1) {
+    throw new ClientError("Film not found", 404);
+  }
+  films[index] = { ...films[index], ...body };
+};
+
+module.exports = {
+  list,
+  create,
+  findById,
+  findByIdAndRemove,
+  findByIdAndUpdate,
+};
