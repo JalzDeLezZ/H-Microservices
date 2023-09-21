e419ad7220e4a434d5e50c01109f922ddef28ab4
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-09-20 22:07:13 -0500

    [add] (Schemas) ✅

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -4,9 +4,31 @@ const { MONGO_URI } = require("../config/enviroments");
 const conn = mongoose.createConnection(MONGO_URI);
 
 const Character = conn.model("Character", require("./schemas/characterSchema"));
+const Film = conn.model("Film", require("./schemas/filmSchema"));
+const Planet = conn.model("Planet", require("./schemas/planetSchema"));
 
-Character.find()
-  .populate("films")
+/* Character.find()
+  // .populate("films")
+  .populate("films", ["_id", "title"])
   .then((characters) => {
-    console.log(characters);
+    console.log(characters[0]);
+  }); */
+
+/* Film.find()
+  .populate("planets", ["_id", "name"])
+  .then((films) => {
+    console.log(films[0]);
+  }); */
+
+Planet.find()
+  .populate("films", ["_id", "title"])
+  .then((planets) => {
+    console.log(planets[0]);
   });
+
+// TEST EXECUTION: node database/src/database/index.js
+
+module.exports = {
+  Character,
+  Film,
+};
